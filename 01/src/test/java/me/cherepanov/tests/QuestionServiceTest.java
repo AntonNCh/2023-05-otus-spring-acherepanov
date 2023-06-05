f05ea387e8fb9685f5e28170d560034258126677
Anton Cherepanov <anton.cherepanov@ecm-consulting.ru>
2023-06-06 01:19:33 +0300

    01 test

--- 01/src/test/java/me/cherepanov/tests/QuestionServiceTest.java
+++ 01/src/test/java/me/cherepanov/tests/QuestionServiceTest.java
@@ -1,15 +1,20 @@
 package me.cherepanov.tests;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("QuestionService Test")
 public class QuestionServiceTest {
-    
 
+    @DisplayName("QuestionService: Check numer of tests == 5")
     @Test
     void QuestionServiceTest(){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
-        Object questionsService = context.getBean("questionsService");
+        me.cherepanov.spring.service.QuestionsServiceImpl questionsService =
+                context.getBean("questionsService",me.cherepanov.spring.service.QuestionsServiceImpl.class);
+        assertEquals(5, questionsService.getSize());
     }
 }
