13de5777802f3c4a1aaf45bd103ec7cd31b70c8e
Anton Cherepanov <anton.cherepanov@ecm-consulting.ru>
2023-06-15 23:40:40 +0300

    tests

---
+++ 02/src/test/java/me/cherepanov/spring/service/io/QuestionServiceTest.java
@@ -0,0 +1,36 @@
+package me.cherepanov.spring.service.io;
+
+import me.cherepanov.spring.dao.CSVReaderQuestionsDAO;
+import me.cherepanov.spring.dao.QuestionsDAO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+@DisplayName("Question Service Test")
+public class QuestionServiceTest {
+
+    @DisplayName("Test the QuestionService with mock")
+    @Test
+    void questionsService() {
+        QuestionsDAO questionDAO = mock(QuestionsDAO.class);
+        QuestionsServiceImpl questionsService = new QuestionsServiceImpl(questionDAO);
+        questionsService.getAllQuestions();
+        assertEquals(0, questionsService.getAllQuestions().size());
+    }
+
+    @DisplayName("Test the QuestionService reading from file")
+    @Test
+    void questionsServiceIntegr() throws IOException {
+        Properties properties = new Properties();
+        properties.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+        QuestionsDAO questionDAO = new CSVReaderQuestionsDAO(properties.getProperty("questionary.file.name"));
+        QuestionsServiceImpl questionsService = new QuestionsServiceImpl(questionDAO);
+        questionsService.getAllQuestions();
+        assertEquals(3, questionsService.getAllQuestions().size());
+    }
+}
